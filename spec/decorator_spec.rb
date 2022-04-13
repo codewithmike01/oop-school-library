c52b06a5550c71b6da382c3d1104dbf4f0948d96
ginohmk <michotall95@gmail.com>
2022-04-13 16:02:34 +0100

    Fix linters error

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../decorator'
 
 describe Nameable do
-  context 'Tesing the Nameable class' do
+  context 'Tesing the Nameable Decorator class' do
     it 'correct_name method should raise an error' do
       nameable = Nameable.new
-      expect { nameable.correct_name }.to raise_error(NotImplementedError) 
+      expect { nameable.correct_name }.to raise_error(NotImplementedError)
     end
   end
 end
