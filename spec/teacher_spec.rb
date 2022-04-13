3afa20719852c08f8132e70fca06b3289210919d
ginohmk <michotall95@gmail.com>
2022-04-13 15:04:39 +0100

    Add teacher test

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../teacher'
+
+describe Teacher do
+  before(:all) do
+    @teacher = Teacher.new(73, 'Liar', 'Jack Dozer')
+  end
+  context 'Tesing the person class' do
+    it 'has instance of person' do
+      expect(@teacher).to be_instance_of Teacher
+    end
+
+    it 'has return value name' do
+      expect(@teacher.age).to eq(73)
+    end
+
+    it 'has return value can use services' do
+      expect(@teacher.name).to eq 'Jack Dozer'
+    end
+  end
+end
